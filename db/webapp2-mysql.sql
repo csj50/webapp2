6c21f26d74db49b2361b6f1686c157dcfa981c47
sjcui <287229112@qq.com>
2019-11-18 11:19:01 +0800

    添加数据库脚本

---
+++ db/webapp2-mysql.sql
@@ -0,0 +1,28 @@
+drop table tbl_teacher_inf;
+
+create table tbl_teacher_inf (
+id int not null,
+name varchar(100),
+primary key (id)
+);
+
+insert into tbl_teacher_inf values (1, '刘备');
+insert into tbl_teacher_inf values (2, '张飞');
+insert into tbl_teacher_inf values (3, '关羽');
+insert into tbl_teacher_inf values (4, '马超');
+insert into tbl_teacher_inf values (5, '黄忠');
+insert into tbl_teacher_inf values (6, '赵云');
+insert into tbl_teacher_inf values (7, '魏延');
+insert into tbl_teacher_inf values (8, '关平');
+insert into tbl_teacher_inf values (9, '周仓');
+insert into tbl_teacher_inf values (10, '关兴');
+insert into tbl_teacher_inf values (11, '张苞');
+insert into tbl_teacher_inf values (12, '陈到');
+insert into tbl_teacher_inf values (13, '李严');
+insert into tbl_teacher_inf values (14, '姜维');
+insert into tbl_teacher_inf values (15, '廖化');
+insert into tbl_teacher_inf values (16, '马谡');
+insert into tbl_teacher_inf values (17, '马岱');
+insert into tbl_teacher_inf values (18, '陈式');
+insert into tbl_teacher_inf values (19, '雷铜');
+insert into tbl_teacher_inf values (20, '吴兰');
